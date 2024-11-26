aa580a89fa2b43e3abc07388a1a2cfc42f39b906
universmotion <romainleuter104@gmail.com>
2024-11-26 15:13:01 +0100

    UPDATE exchange file of init var

--- src/app/app/streamlit_utils/cache_ressource.py
+++ src/app/app/streamlit_utils/cache_ressource.py
@@ -1,8 +1,24 @@
-from utils.db_vector import DB_VECTORIAL
+from utils.db_vector import DBVector
 import streamlit as st
+import logging
+import os
+
+logger = logging.getLogger(__name__)
 
 
 @st.cache_resource
 def get_vectorial_db():
-    db_vectorial = DB_VECTORIAL.get_db()
-    return db_vectorial
+    try:
+        option_db = eval(os.environ["OPTION_DB_VECTOR"])
+        option_host = eval(os.environ["HOST_DB_VECTOR"])
+        if isinstance(option_db, dict) and isinstance(option_host, list):
+            return DBVector(
+                option_db=option_db,
+                option_host=option_host
+            ).get_db()
+        else:
+            raise ValueError(
+                f"the type of option_db is {type(option_db)} and the type of option_host is {type(option_host)}")
+    except RuntimeError as e:
+        logger.critical(f"Failed to initialize database managers: {e}")
+        raise
--- src/app/app/utils/db_vector.py
+++ src/app/app/utils/db_vector.py
@@ -17,25 +17,11 @@ class DBVector:
         except Exception:
             logging.error(
                 f"Error to initialized vectorial db: {traceback.format_exc()}")
+            raise
 
     def get_db(self):
         try:
             return self.client
         except Exception:
             logger.error(traceback.format_exc())
-
-
-try:
-    option_db = eval(os.environ["OPTION_DB_VECTOR"])
-    option_host = eval(os.environ["HOST_DB_VECTOR"])
-    if isinstance(option_db, dict) and isinstance(option_host, list):
-        DB_VECTORIAL = DBVector(
-            option_db=option_db,
-            option_host=option_host
-        )
-    else:
-        raise ValueError(
-            f"the type of option_db is {type(option_db)} and the type of option_host is {type(option_host)}")
-except RuntimeError as e:
-    logger.critical(f"Failed to initialize database managers: {e}")
-    raise
+            raise
