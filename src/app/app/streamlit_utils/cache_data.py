928061de63f5f143a8492fd269f130e90e323f7a
universmotion <romainleuter104@gmail.com>
2025-01-06 12:23:32 +0100

    UPDATE col name in BD

--- src/app/app/streamlit_utils/cache_data.py
+++ src/app/app/streamlit_utils/cache_data.py
@@ -13,9 +13,9 @@ def get_post_db():
     posts = post_db.execute(text("""
             select p."id", p."content", p.title, p.created_at, p.updated_at, 
             p.author_name, d."name" as departement_name, 
-            d."number" as number_departement
+            d."code" as number_departement
             from postgres.public.posts p
-            left join postgres.public.departments d on d.id = p.department_id 
+            left join postgres.public.departments d on d.code = p.department_code 
             left join postgres.public.user_profiles up on up.id = p.user_profile_id 
             where p.emitor_status = 'COMMENDATAIRE'
         """))
